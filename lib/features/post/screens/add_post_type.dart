c906406e4cdac5a687b3e49a666a753fff07c79b
Prasad K S <prasad07062001@gmail.com>
2023-09-23 15:42:54 +0530

    add textfield validation in post type

--- lib/features/post/screens/add_post_type.dart
+++ lib/features/post/screens/add_post_type.dart
@@ -1,6 +1,15 @@
-import 'package:flutter/cupertino.dart';
+// ignore_for_file: deprecated_member_use
+
+import 'dart:io';
+
+import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:reddit_clone/core/common/error_text.dart';
+import 'package:reddit_clone/core/utils.dart';
+import 'package:reddit_clone/features/community/controller/community_controller.dart';
+import 'package:reddit_clone/models/community_model.dart';
+import 'package:reddit_clone/theme/pallete.dart';
 
 class AddPostTypeScreen extends ConsumerStatefulWidget {
   const AddPostTypeScreen({required this.type, super.key});
@@ -11,8 +20,224 @@ class AddPostTypeScreen extends ConsumerStatefulWidget {
 }
 
 class _AddPostTypeScreenState extends ConsumerState<AddPostTypeScreen> {
+  final titleController = TextEditingController();
+  final desController = TextEditingController();
+  final linkController = TextEditingController();
+
+  File? bannerFile;
+  List<Community> communities = [];
+  Community? selectedCommunity;
+
+  final myKey = GlobalKey<FormState>();
+
+  @override
+  void dispose() {
+    titleController.dispose();
+    linkController.dispose();
+    desController.dispose();
+    super.dispose();
+  }
+
+  void selectBannerImage() async {
+    final res = await pickImage();
+    if (res != null) {
+      setState(() {
+        bannerFile = File(res.files.first.path!);
+      });
+    }
+  }
+
+  void sharePost() {
+    final isValid = myKey.currentState!.validate();
+    if (isValid) {}
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    final currentTheme = ref.watch(themeNotifierProvider);
+    final isTypeImage = widget.type == 'image';
+    final isTypeText = widget.type == 'text';
+    final isTypeLink = widget.type == 'link';
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('Post ${widget.type}'),
+        actions: [
+          TextButton(
+            onPressed: sharePost,
+            child: const Text('Share'),
+          ),
+        ],
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Form(
+            key: myKey,
+            child: Column(
+              children: [
+                TextFormField(
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter the title';
+                    }
+                    return null;
+                  },
+                  textInputAction: isTypeImage ? TextInputAction.done : TextInputAction.next,
+                  controller: titleController,
+                  maxLength: 30,
+                  decoration: InputDecoration(
+                    errorStyle: const TextStyle(
+                      fontSize: 16,
+                    ),
+                    filled: true,
+                    hintText: 'Enter Title here',
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: const BorderSide(
+                        color: Colors.blue,
+                      ),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    border: InputBorder.none,
+                    contentPadding: const EdgeInsets.all(18),
+                  ),
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                if (isTypeImage)
+                  GestureDetector(
+                    onTap: selectBannerImage,
+                    child: DottedBorder(
+                      borderType: BorderType.RRect,
+                      radius: const Radius.circular(10),
+                      dashPattern: const [10, 4],
+                      strokeCap: StrokeCap.round,
+                      color: currentTheme.textTheme.bodyText2!.color!,
+                      child: Container(
+                        width: double.infinity,
+                        height: 150,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: bannerFile != null
+                            ? Image.file(
+                                bannerFile!,
+                                fit: BoxFit.cover,
+                              )
+                            : const Center(
+                                child: Icon(
+                                  Icons.camera_alt_outlined,
+                                  size: 40,
+                                ),
+                              ),
+                      ),
+                    ),
+                  ),
+                if (isTypeText)
+                  TextFormField(
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter the description';
+                      }
+                      return null;
+                    },
+                    controller: desController,
+                    textInputAction: TextInputAction.done,
+                    decoration: InputDecoration(
+                      errorStyle: const TextStyle(
+                        fontSize: 16,
+                      ),
+                      filled: true,
+                      hintText: 'Enter Description here',
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(
+                          color: Colors.blue,
+                        ),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      border: InputBorder.none,
+                      contentPadding: const EdgeInsets.all(18),
+                    ),
+                    maxLines: 5,
+                  ),
+                if (isTypeLink)
+                  TextFormField(
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter the link';
+                      }
+                      return null;
+                    },
+                    controller: desController,
+                    onTapOutside: (event) {
+                      FocusScope.of(context).unfocus();
+                    },
+                    textInputAction: TextInputAction.done,
+                    decoration: InputDecoration(
+                      errorStyle: const TextStyle(
+                        fontSize: 16,
+                      ),
+                      filled: true,
+                      hintText: 'Enter link here',
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(
+                          color: Colors.blue,
+                        ),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      border: InputBorder.none,
+                      contentPadding: const EdgeInsets.all(18),
+                    ),
+                  ),
+                const SizedBox(
+                  height: 20,
+                ),
+                const Align(
+                  alignment: Alignment.topLeft,
+                  child: Text('Select Communtiy'),
+                ),
+                ref.watch(userCommunityProvider).when(
+                  data: (data) {
+                    communities = data;
+                    if (data.isEmpty) {
+                      return const Text('You haven\'t joined any community.');
+                    }
+                    return DropdownButton(
+                      value: selectedCommunity ?? data[0],
+                      items: data
+                          .map(
+                            (e) => DropdownMenuItem(
+                              value: e,
+                              child: Text(e.name),
+                            ),
+                          )
+                          .toList(),
+                      onChanged: (value) {
+                        setState(() {
+                          selectedCommunity = value;
+                        });
+                      },
+                    );
+                  },
+                  error: (error, stackTrace) {
+                    return ErrorText(
+                      error: error.toString(),
+                    );
+                  },
+                  loading: () {
+                    return Center(
+                      child: CircularProgressIndicator.adaptive(
+                        backgroundColor: Colors.orange.shade900,
+                      ),
+                    );
+                  },
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
